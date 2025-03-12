bb66e07fbba1619443bfe18c99f0f0d78298a72b
mcdanielspecarina278 <mcdanielspecarina278@gmail.com>
2025-03-12 07:57:28 +0000

    Add 7.rs

---
+++ src/7.rs
@@ -0,0 +1,8 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+    scores.insert("Alice", 10);
+    scores.insert("Bob", 20);
+    scores.insert("Charlie", 30);
+}
