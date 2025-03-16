959ee44bd964375fffcae3e95223454f04859a99
mcdanielspecarina278 <mcdanielspecarina278@gmail.com>
2025-03-16 22:10:00 +0000

    Add 11.rs

---
+++ src/11.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut grades = HashMap::new();
+    grades.insert(String::from("Math"), 92);
+    grades.insert(String::from("Science"), 85);
+    grades.insert(String::from("History"), 76);
+    grades.insert(String::from("English"), 90);
+}
